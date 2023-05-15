1fdd7324597b91c2bb2705c9a05bd176bacab687
shashwat329 <56999868+shashwat329@users.noreply.github.com>
2023-05-16 02:36:07 +0530

    Create scrpit.js
    This is the a javascript file of a basic image-slider with navigation button and dot indicator.

---
+++ scrpit.js
@@ -0,0 +1,33 @@
+let slideIndex = 1;
+showSlide(slideIndex);
+
+function nextSlide() {
+  showSlide(slideIndex += 1);
+}
+
+function prevSlide() {
+  showSlide(slideIndex -= 1);
+}
+
+function currentSlide(n) {
+  showSlide(slideIndex = n);
+}
+
+function showSlide(n) {
+  let slides = document.getElementsByClassName("slider")[0].getElementsByTagName("img");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
